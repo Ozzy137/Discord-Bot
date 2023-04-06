84753e9b00b1d3b669c76a3085ce87ed2f292fab
Ozzy137 <65245929+Ozzy137@users.noreply.github.com>
2023-04-05 20:31:35 -0700

    Add files via upload

---
+++ Bot.cs
@@ -0,0 +1,58 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using DSharpPlus.Interactivity;
+using DSharpPlus.Interactivity.Extensions;
+using DSharpPlus.Net.Serialization;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvanBot
+{
+    public class Bot
+    {
+        public DiscordClient Client { get; private set; }
+        public InteractivityExtension Interactivity { get; private set; }
+
+        public CommandsNextExtension Commands { get; private set; }
+
+        public async Task RunAsync()
+        {
+
+            var configJsonFile = new JsonReader();
+            await configJsonFile.ReadJson();
+
+            var config = new DiscordConfiguration()
+            {
+                Token = configJsonFile.token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+            };
+
+            Client = new DiscordClient(config);
+            Client.UseInteractivity(new InteractivityConfiguration()
+            {
+                Timeout = TimeSpan.FromMinutes(2)
+            });
+
+            var commandConfig = new CommandsNextConfiguration()
+            {
+                StringPrefixes = new string[] { configJsonFile.prefix },
+                EnableMentionPrefix = true,
+                EnableDms = true,
+            };
+
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+        private Task OnClientReady(ReadyEventArgs e)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ configJsonReader.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvanBot
+{
+    internal class JsonReader
+    {
+        public string token { get; set; }
+        public string prefix { get; set; }
+
+        public JsonReader()
+        {
+
+        }
+
+        public async Task ReadJson()
+        {
+            using (StreamReader sr = new StreamReader("config.json", new UTF8Encoding(false)))
+            {
+                string json = await sr.ReadToEndAsync();
+                ConfigJSON obj = JsonConvert.DeserializeObject<ConfigJSON>(json);
+
+                this.token = obj.Token;
+                this.prefix = obj.Prefix;
+            }
+        }
+
+    }
+}
